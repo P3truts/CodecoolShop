58566cbce221cecc1aae00e2df938f3645946d81
Octavian Dinu <dinuoctavianstefan@gmail.com>
2022-08-31 15:47:22 +0300

    Get data from form to Chekout Controller

--- src/Codecool.CodecoolShop/Controllers/CheckoutController.cs
+++ src/Codecool.CodecoolShop/Controllers/CheckoutController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Codecool.CodecoolShop.Helpers;
+using Codecool.CodecoolShop.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 
 namespace Codecool.CodecoolShop.Controllers
 {
     public class CheckoutController : Controller
     {
+        private readonly ILogger<CheckoutController> logger;
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public CheckoutController(ILogger<CheckoutController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                Account.GetInstance().FirstName = collection["FName"];
+                Account.GetInstance().LastName = collection["LName"];
+                Account.GetInstance().PhoneNumber = collection["Phone"];
+                Account.GetInstance().Email = collection["Email"];
+                Account.GetInstance().ShippingAddress = collection["City"] + collection["Country"] + collection["ZipCode"] + collection["Address"];
+                Account.GetInstance().BillingAddress = Account.GetInstance().ShippingAddress;
+                
+                Order.GetInstance().Account = Account.GetInstance();
+                Order.GetInstance().Items = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+
+                return View("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
---
+++ src/Codecool.CodecoolShop/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.Models
+{
+    public class Order
+    {
+        private static Order instance = null;
+        public static Guid Id { get; set; }
+
+        public Account Account { get; set; }
+
+        public List<Item> Items { get; set; }
+
+        public static Order GetInstance()
+        {
+            Id = new Guid();
+            if(instance == null)
+                instance = new Order();
+            return instance;
+        }
+    }
+}
