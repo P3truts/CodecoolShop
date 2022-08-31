2602640f21c6628ab75daa65994c190634c77ea4
Octavian Dinu <dinuoctavianstefan@gmail.com>
2022-08-31 13:41:16 +0300

    Add checkout controller

---
+++ src/Codecool.CodecoolShop/Controllers/CheckoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
