b6e3bbda51c93dbdadc8ba876780c22a3663a033
P3truts <obreja.petrut@gmail.com>
2022-09-04 10:44:13 +0300

    add async page load functions

--- src/Codecool.CodecoolShop/wwwroot/js/site.js
+++ src/Codecool.CodecoolShop/wwwroot/js/site.js
@@ -18,11 +18,48 @@ function changeQuantity(id) {
     location.href = `https://localhost:44368/cart/buy/${id}?quantity=${qtyDiv.innerText}`;
 }
 
-function addToCart(id) { //work in progress
-    $.ajax({
-        url: 'https://localhost:44368/cart/buy/id',
-        data: { id: id }
-    }).done(function () {
-        alert('Added');
-    });
+async function postData(url = '') {
+    const response = await fetch(url);
+    return response.text();
+}
+
+function changeQuantityAsync(id) {
+    var qtyDiv = document.getElementById(`qty-div-${id}`);
+    const container = document.getElementById("cart-container");
+    postData(`https://localhost:44368/cart/buy/${id}?quantity=${qtyDiv.innerText}`)
+        .then((data) => {
+            container.innerHTML = data;
+        });
+}
+
+function addToCart(id) {
+    const container = document.getElementById("cart-container");
+    postData(`https://localhost:44368/cart/buy/${id}`)
+        .then((data) => {
+            container.innerHTML = data;
+        });
+}
+
+function removeFromCart(id) {
+    const container = document.getElementById("cart-container");
+    postData(`https://localhost:44368/cart/remove/${id}`)
+        .then((data) => {
+            container.innerHTML = data;
+        });
+}
+
+function filterProductsBySupplier(id) {
+    const container = document.getElementById("products-container");
+    postData(`https://localhost:44368/Index/Products/?supplier=${id}`)
+        .then((data) => {
+            container.innerHTML = data;
+        });
+}
+
+function filterProductsByCategory(id) {
+    const container = document.getElementById("products-container");
+    postData(`https://localhost:44368/Index/Products/?category=${id}`)
+        .then((data) => {
+            container.innerHTML = data;
+        });
 }
