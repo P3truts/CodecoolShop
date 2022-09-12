8b206708137d715ba5fbb37e4688afe11ea3b27d
P3truts <obreja.petrut@gmail.com>
2022-09-12 16:21:49 +0300

    refactor to test ProductService

--- tests/Codecool.CodecoolShop.UnitTests/ServiceTest.cs
+++ tests/Codecool.CodecoolShop.UnitTests/ServiceTest.cs
@@ -1,4 +1,5 @@
 using Codecool.CodecoolShop.Models;
+using Codecool.CodecoolShop.Services;
 using NSubstitute;
 using System.Drawing;
 
@@ -6,28 +7,32 @@ namespace Codecool.CodecoolShop.UnitTests
 {
     public class Tests
     {
-        private IProductDao productDao;
-        private IProductCategoryDao productCategoryDao;
-        private ISupplierDao supplierDao;
+        private IProductDao _productDao;
+        private IProductCategoryDao _productCategoryDao;
+        private ISupplierDao _supplierDao;
+        private ProductService _productService;
 
         [SetUp]
         public void Setup()
         {
-            productDao = ProductDaoMemory.GetInstance();
-            productCategoryDao = ProductCategoryDaoMemory.GetInstance();
-            supplierDao = SupplierDaoMemory.GetInstance();
+            _productDao = ProductDaoMemory.GetInstance();
+            _productCategoryDao = ProductCategoryDaoMemory.GetInstance();
+            _supplierDao = SupplierDaoMemory.GetInstance();
+
+            _productService = new ProductService(_productDao, _productCategoryDao, _supplierDao);
+
 
             Supplier amazon = new Supplier { Name = "Amazon", Description = "Digital content and services" };
-            supplierDao.Add(amazon);
+            _supplierDao.Add(amazon);
             Supplier lenovo = new Supplier { Name = "Lenovo", Description = "Computers" };
-            supplierDao.Add(lenovo);
+            _supplierDao.Add(lenovo);
 
             ProductCategory tablet = new ProductCategory { Name = "Tablets", Department = "Hardware", Description = "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display." };
-            productCategoryDao.Add(tablet);
+            _productCategoryDao.Add(tablet);
 
-            productDao.Add(new Product { Name = "Amazon Fire", DefaultPrice = 49.9m, Currency = "EUR", Description = "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", ProductCategory = tablet, Supplier = amazon });
-            productDao.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "EUR", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
-            productDao.Add(new Product { Name = "Amazon Fire HD 8", DefaultPrice = 89.0m, Currency = "EUR", Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", ProductCategory = tablet, Supplier = amazon });
+            _productDao.Add(new Product { Name = "Amazon Fire", DefaultPrice = 49.9m, Currency = "EUR", Description = "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", ProductCategory = tablet, Supplier = amazon });
+            _productDao.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "EUR", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
+            _productDao.Add(new Product { Name = "Amazon Fire HD 8", DefaultPrice = 89.0m, Currency = "EUR", Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", ProductCategory = tablet, Supplier = amazon });
         }
 
         [Test]
@@ -35,16 +40,20 @@ namespace Codecool.CodecoolShop.UnitTests
         {
             var expectedResult = 3;
 
-            Assert.That(expectedResult, Is.EqualTo(productDao.GetAll().Count()));
+            Assert.That(expectedResult, Is.EqualTo(_productService.GetAllProducts().Count()));
         }
 
         [Test]
         public void GetAllProductsEmptyReturnsEmpty()
         {
             var emptyProductDao = Substitute.For<IProductDao>();
+            var emptyCategoryDao = Substitute.For<IProductCategoryDao>();
+            var emptySupplierDao = Substitute.For<ISupplierDao>();
+            var mockProductService = new ProductService(emptyProductDao, emptyCategoryDao, emptySupplierDao);
+
             List<Product> expectedResult = new List<Product>();
 
-            Assert.That(emptyProductDao.GetAll(), Is.EqualTo(expectedResult));
+            Assert.That(mockProductService.GetAllProducts(), Is.EqualTo(expectedResult));
         }
     }
 }
